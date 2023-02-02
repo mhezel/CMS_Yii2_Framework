dbc09d26d83b413f3ff174c657abc022055c3dcc
Mhezel Mohammad <mhezelkhan@gmail.com>
2023-02-02 17:07:06 +0800

    amend changes to sidebar

--- frontend/views/layouts/sidebar.php
+++ frontend/views/layouts/sidebar.php
@@ -1,14 +1,13 @@
 <?php
 ?>
 <!-- Sidebar  -->
-<aside class="shadow">
+<aside class="shadow d-flex">
     <nav id="sidebar" class="active">
         <div class="sidebar-header">
-            <h3>Bootstrap Sidebar</h3>
+            <h3>Content Management System</h3>
         </div>
         <ul class="list-unstyled components">
-            <p>Dummy Heading</p>
-            <li class="active">
+            <li>
                 <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
                 <ul class="collapse list-unstyled" id="homeSubmenu">
                     <li>
